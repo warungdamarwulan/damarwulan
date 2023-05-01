cdab71b482d22d266d87738e067184a38581fd24
prakasita <prakasitawindriya@gmail.com>
2023-05-02 00:17:02 +0700

    update search form timbul

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+
+// Toggle class active untuk search form
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
 };
 
 //klik diluar sidebar untuk menghiangkan nav
